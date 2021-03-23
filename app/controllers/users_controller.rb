564c2706374cb8d1cf9d1409e78b1e8c8f707da9
Ryan Huddleston <wryanhuddleston@gmail.com>
2021-03-23 22:49:48 +0900

    added dates and fixed some n+1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(participations: { event: :episode }).find(params[:id])
     authorize @user
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_065109) do
+ActiveRecord::Schema.define(version: 2021_03_23_121300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2021_03_20_065109) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.date "date"
+    t.datetime "date"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
